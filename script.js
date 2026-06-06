47cc17bf1419cf2cbc3e19578455654f5a8c266f
narayan kosloski <yankoslosk@gmail.com>
2026-06-06 00:24:59 -0300

    scroll

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ window.addEventListener('scroll', () => {
     `translateY(-50%) rotate(${scroll * 0.35}deg)`;
 
 });
+
+
+const knob = document.querySelector('.fader-knob');
+
+window.addEventListener('scroll', () => {
+
+    const scrollTop = window.scrollY;
+
+    const docHeight =
+    document.documentElement.scrollHeight -
+    window.innerHeight;
+
+    const percent = scrollTop / docHeight;
+
+    const trackHeight = 278;
+
+    knob.style.top =
+    `${percent * trackHeight}px`;
+
+});
